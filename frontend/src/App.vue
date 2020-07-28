f591da2806ef1b3b6e4e95555d4137900496ef70
Marcelo Lino <mdslino@gmail.com>
2020-07-28 17:23:26 -0300

    Fix frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,15 @@
 <template lang="pug">
-    div#app
-        div#nav
-            router-link(to="/") Home
-            | |
-            router-link(to="/about") About
+    div#app.container.is-fluid
+        Navigation
         router-view
 </template>
+<script>
+import Navigation from "@/components/Navbar";
 
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    #nav {
-        padding: 30px;
-    }
-
-    #nav a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
-</style>
+export default {
+  name: "app",
+  components: {
+    Navigation: Navigation
+  }
+};
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    b-navbar
+        template(slot="brand")
+            b-navbar-item(tag="router-link", :to="{ path: '/' }")
+                img(src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png",
+                    alt="Lightweight UI components for Vue.js based on Bulma")
+        template(slot="start")
+            b-navbar-item(href="#") Inicio
+            b-navbar-item(href="#") Documentação
+            b-navbar-dropdown(label="Informação" )
+                b-navbar-item(href="#") Sobre
+                b-navbar-item(href="#") Contato
+        template(slot="end")
+            b-navbar-item(tag="end")
+                b-navbar-item(tag="end")
+                    .buttons
+                        a.button.is-primary
+                            strong Registrar
+                        a.button.is-light Entrar
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navigation extends Vue {}
+</script>
